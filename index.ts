98b1943fe2bb81e317513348a3638c2bd14fb6a9
cejuo <101661143+cejuo@users.noreply.github.com>
2022-03-29 17:57:17 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { format, createLogger, transports } from "winston";
+
+const logFormat = format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: format.combine(format.colorize(), format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.ms" }), logFormat),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+export default logger;
